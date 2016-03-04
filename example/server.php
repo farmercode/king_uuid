f59835986839ecf40b55beef2597c9e48c8232f0
farmercode <wangchangchun120@gmail.com>
2016-03-04 23:47:30 +0800

    swoole uuid简单例子

---
+++ example/server.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *配合swoole做的uuid服务
+ */
+$server_config = [
+    'worker_num' => 4,
+    'max_request'  => 10000
+];
+$server_host = '0.0.0.0';
+$server_port = 6957;
+$sw_server = new swoole_http_request($server_host,$server_port);
+$sw_server->set($server_config);
+
+$sw_server->on('Request',function($request,$response){
+    $data['uuid'] = ku_get_uuid();
+    $response->end(json_encode($data));
+});
